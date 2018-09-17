4231a489427124aa9c2ba25243c5fe8244f3e343
ktanaha <moutoku1984@gmail.com>
2018-09-17 18:36:51 +0900

    add watch

--- directive/js/index.js
+++ directive/js/index.js
@@ -1,14 +1,25 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Template!"
+    km: 0,
+    m: 0,
+    mm: 0
   },
-  methods: {
-    clickHandler: function(event) {
-      this.message = this.message
-        .split("")
-        .reverse()
-        .join("");
+  watch: {
+    km: function(value) {
+      this.km = value;
+      this.m = value * 1000;
+      this.mm = value * 1000000;
+    },
+    m: function(value) {
+      this.km = value / 1000;
+      this.m = value;
+      this.mm = value * 1000;
+    },
+    mm: function(value) {
+      this.km = value / 1000000;
+      this.m = value / 1000;
+      this.mm = value;
     }
   }
 });

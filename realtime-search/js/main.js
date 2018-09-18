66b4f9cec814c64ee8c6631fe853552e8e26f3f8
ktanaha <moutoku1984@gmail.com>
2018-09-19 07:35:41 +0900

    test api

--- realtime-search/js/main.js
+++ realtime-search/js/main.js
@@ -1,3 +1,39 @@
 var app = new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    items: null,
+    keyword: "",
+    message: ""
+  },
+  watch: {
+    url: function() {}
+  },
+  created: function() {
+    this.keyword = "JavaScript";
+    this.getAnswer();
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === "") {
+        this.item = null;
+        return;
+      }
+
+      this.message = "Loading...";
+
+      var vm = this;
+      var params = { page: 1, per_page: 20, query: this.keyword };
+      axios
+        .get("http://qiita.com/api/v2/items", { params })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          vm.message = "Error" + error;
+        })
+        .finally(function() {
+          this.message = "";
+        });
+    }
+  }
 });
